fb779fd572f822e2b5bee07734f8b1da8c34ba22
aleksei-g <aleksei_g@outlook.com>
2016-12-06 20:17:11 +0300

    tag template moved into html from js

--- js/main.js
+++ js/main.js
@@ -27,21 +27,7 @@ function ShowNotifyMessage() {
   },
   delay: 0,
   mouse_over: 'pause',
-  template:
-  '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
-      '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
-      '<span data-notify="icon"></span> ' +
-      '<span data-notify="title">{1}</span> ' +
-      '<span data-notify="message">{2}</span>' +
-      '<div class="text-center">'+
-        '<a class="btn btn-info btn-sm" data-notify="dismiss">ДА</a>' +
-        '<a class="btn btn-info btn-sm" data-notify="dismiss" id="button_error_region">НЕТ</a>' +
-      '</div>'+
-      '<div class="progress" data-notify="progressbar">' +
-          '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0;"></div>' +
-      '</div>' +
-      '<a href="{3}" target="{4}" data-notify="url"></a>' +
-  '</div>'
+  template: AlertTemplate
   });
 }
 
